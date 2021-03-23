558654d4672c6bb80b83acbb11dd7ff828e772b9
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-03-23 16:54:42 -0600

    style navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,14 @@ import Navbar from 'react-bootstrap/Navbar';
 function NavbarEl() {
 	return (
 		<>
-			<Navbar bg="dark" variant="dark">
+			<Navbar bg="dark" variant="dark" className="justify-content-center">
 				<Navbar.Brand href="#home">
 					<img
 						alt=""
 						src="./logo512.png"
 						width="30"
 						height="30"
-						className="d-inline-block align-top"
+						className="d-inline-block align-top me-3"
 					/>{' '}
 					Employee Directory
 				</Navbar.Brand>
