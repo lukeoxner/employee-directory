1cb997aaefbb9d8d616b3f9bf7208e5f812a6741
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-03-23 22:01:02 -0600

    formatting for employee cards

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -10,17 +10,20 @@ function EmployeeCard(props) {
 			<>
 				<ul className="list-group search-results">
 					{props.results.map((result) => (
-						<li key={result.cell} className="list-group-item">
-							<img
-								alt="employee"
-								src={result.picture.thumbnail}
-								className="img-fluid"
-							/>
-							<p>
-								{result.name.first} {result.name.last}
-							</p>
-							<p>{result.email}</p>
-							<p>{result.cell}</p>
+						<li key={result.cell} className="list-group-item container">
+							<div className="row">
+								<div className="col-2">
+									<img
+										alt="employee"
+										src={result.picture.thumbnail}
+										className="img-fluid"
+									/>
+								</div>
+								<p className="col-3">
+									{result.name.first} {result.name.last}
+								</p>
+								<p className="col-5">{result.email}</p>
+							</div>
 						</li>
 					))}
 				</ul>
