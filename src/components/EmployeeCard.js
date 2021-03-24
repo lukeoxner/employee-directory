a4ffd2188fc8080444b1c8ed0ec3c3caf2529ac3
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-03-24 00:08:40 -0600

    work on sort functionality

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -1,12 +1,53 @@
 import React from 'react';
 
 function EmployeeCard(props) {
-	console.log(props.results);
-	console.log(props.search);
-
 	if (!props.results) {
 		return null;
-	} else if (props.results && !props.search) {
+	} else if (props.results && !props.search && !props.sort) {
+		return (
+			<>
+				<ul className="list-group search-results">
+					{props.results.map((result) => (
+						<li key={result.cell} className="list-group-item container">
+							<div className="row">
+								<div className="col-2">
+									<img
+										alt="employee"
+										src={result.picture.thumbnail}
+										className="img-fluid"
+									/>
+								</div>
+								<p className="col-3">
+									{result.name.first} {result.name.last}
+								</p>
+								<p className="col-5">{result.email}</p>
+							</div>
+						</li>
+					))}
+				</ul>
+			</>
+		);
+	} else if (props.results && !props.search && props.sort) {
+		if (props.sort === 'asc') {
+			// sort by ascending a-z
+			props.results.sort((a, b) => {
+				if (a.name.first < b.name.first) {
+					return -1;
+				} else if (a.name.first > b.name.first) {
+					return 1;
+				}
+			});
+		} else {
+			// sort by descending z-a
+			props.results.sort((a, b) => {
+				if (a.name.first < b.name.first) {
+					return 1;
+				} else if (a.name.first > b.name.first) {
+					return -1;
+				}
+			});
+		}
+
 		return (
 			<>
 				<ul className="list-group search-results">
@@ -38,8 +79,6 @@ function EmployeeCard(props) {
 
 			return name.indexOf(props.search.toLowerCase()) !== -1;
 		});
-
-		console.log(searchedResults);
 
 		return (
 			<>
--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -6,17 +6,25 @@ function EmployeeTable(props) {
 	// console.log(props);
 
 	const [search, setSearch] = useState('');
+	const [sort, setSort] = useState('');
 
 	const handleInputChange = (e) => {
 		setSearch(e.target.value);
-		// console.log(search);
 	};
+
+	function sortAsc() {
+		setSort('asc');
+	}
+
+	function sortDesc() {
+		setSort('desc');
+	}
 
 	return (
 		<>
 			<div className="container">
 				<div className="row justify-content-center">
-					<div className="col-md-10">
+					<div className="col-8">
 						<form className="search my-3">
 							<div className="form-group">
 								<label htmlFor="language">Search Employees:</label>
@@ -35,14 +43,18 @@ function EmployeeTable(props) {
 					</div>
 					<div className="row mb-3 justify-content-center">
 						<div className="col-3">
-							<Button variant="dark">Sort Ascending (A to Z)</Button>{' '}
+							<Button onClick={sortAsc} variant="dark">
+								Sort Ascending (A to Z)
+							</Button>{' '}
 						</div>
 						<div className="col-3">
-							<Button variant="dark">Sort Descending (Z to A)</Button>{' '}
+							<Button onClick={sortDesc} variant="dark">
+								Sort Descending (Z to A)
+							</Button>{' '}
 						</div>
 					</div>
 					<div className="row">
-						<EmployeeCard results={props.results} search={search} />
+						<EmployeeCard results={props.results} search={search} sort={sort} />
 					</div>
 				</div>
 			</div>
